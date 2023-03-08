bd0ef93f882731921f4bd481cdf6b9ed5dfecafc
tilouage <titus.louage@ugent.be>
2023-03-08 11:03:52 +0100

    small changes

--- src/client/Node.tsx
+++ src/client/Node.tsx
@@ -10,6 +10,7 @@ interface NodeProps {
     color: string;
     backgroundColor: string;
     fill: boolean;
+    size: number;
 }
 
 export class NodeElement extends React.Component<NodeProps, {}> {
@@ -20,11 +21,13 @@ export class NodeElement extends React.Component<NodeProps, {}> {
                     <div className='iconWrapper' style={{
                         borderColor: this.props.color,
                         backgroundColor: this.props.fill ? this.props.color : this.props.backgroundColor,
+                        width: this.props.size,
+                        height: this.props.size
                         }}>
                         <Icon path={this.props.icon} color={this.props.iconColor} />
                     </div>
                     
-                    <p className='name'>{this.props.name}</p>
+                    <p className='name' style={{backgroundColor: this.props.backgroundColor}}>{this.props.name}</p>
                 </div>
             </div>
             
