ea665afc29b6e7db855e2bc899f58a709b7f2224
tilouage <titus.louage@ugent.be>
2023-03-15 14:15:02 +0100

    Icon now hook

--- src/client/Icon.tsx
+++ src/client/Icon.tsx
@@ -5,17 +5,17 @@ interface IconProps {
     color: string;
 }
 
-export default class Icon extends React.Component<IconProps, {}> {
-    render() {
-        const svgFile = this.props.path.split('/')[0];
-        const svgId = this.props.path.split('/')[1];
-        const iconSheet = require('../icons/material.svg').default;
+const Icon = ({path, color}: IconProps) => {
+    const svgFile = path.split('/')[0];
+    const svgId = path.split('/')[1];
+    const iconSheet = require('../icons/material.svg').default;
 
-        return (
-            <div className='icon' style={{
-                maskImage: `url(${iconSheet}#${svgId})`,
-                backgroundColor: this.props.color
-            }}></div>
-        );
-    }
+    return (
+        <div className='icon' style={{
+            maskImage: `url(${iconSheet}#${svgId})`,
+            backgroundColor: color
+        }}></div>
+    );
 }
+
+export default Icon;
